ef0cfd04e86d18502f19d1670a42de63d75c994a
Björn Schießle <schiessle@owncloud.com>
2016-05-23 11:48:14 +0200

    create a real federated share if a user add a public link to his ownCloud
    this way the owner sees all mounted public links and control them individually

--- apps/federatedfilesharing/appinfo/routes.php
+++ apps/federatedfilesharing/appinfo/routes.php
@@ -19,22 +19,8 @@
  *
  */
 
-$app = new \OCA\FederatedFileSharing\AppInfo\Application();
-
-use OCA\FederatedFileSharing\Notifier;
-
-$l = \OC::$server->getL10N('files_sharing');
-
-$app->registerSettings();
-
-$manager = \OC::$server->getNotificationManager();
-$manager->registerNotifier(function() {
-	return new Notifier(
-		\OC::$server->getL10NFactory()
-	);
-}, function() use ($l) {
-	return [
-		'id' => 'files_sharing',
-		'name' => $l->t('Federated sharing'),
-	];
-});
+return [
+	'routes' => [
+		['name' => 'SaveToOwnCloud#saveToOwnCloud', 'url' => '/saveToOwnCloud', 'verb' => 'POST'],
+	]
+];
